f222bb75491f8541ca61a7e7bcb94ba509074e68
VerenaVertonghen <verena.vertonghen@optis.be>
2015-05-18 20:26:36 +0200

    Correct manifest.yml, db settings bluemix, correct package.json.

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost/myproject' 
+    'url' : 'mongodb://8d585f45-ac16-4251-ad1b-64e2f14127f6:d7bb5788-f40d-4173-89ab-2d95c4578901@159.8.128.78:10119/db' 
 
 };
